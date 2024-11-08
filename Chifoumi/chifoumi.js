3895894e519511c108d27727f733a0cfd3911a20
NapoluonJR <beaumann.lukas@outlook.fr>
2024-11-08 16:35:19 +0100

    chifoumi pokemon finalité

--- Chifoumi/chifoumi.js
+++ Chifoumi/chifoumi.js
@@ -2,25 +2,24 @@
 const feu = document.getElementById('feu');
 const eau = document.getElementById('eau');
 const plante = document.getElementById('plante');
-const rejouer = document.getElementById('.rejouer');
-const choixjoueur = document.getElementById('.choixjoueur');
-const choixordinateur = document.getElementById('.choixordinateur');
-const section = document.getElementById('.section');
+const rejouer = document.querySelector('.rejouer');
+const choixjoueur = document.querySelector('.choixjoueur');
+const choixordinateur = document.querySelector('.choixordinateur');
 
-//Ici on a un tableau qui contient les choix
-const choix = ['feu','eau','plante'];
+// Ici on a un tableau qui contient les choix
+const choix = ['feu', 'eau', 'plante'];
 
-//La je créer une fonction pour générer un choix aléatoire pour l'ordi
+// La je créer une fonction pour générer un choix aléatoire pour l'ordi
 function choixOrdinateurAleatoire() {
     const randomIndex = Math.floor(Math.random() * 3);
     return choix[randomIndex];
 }
 
-//C'est pour afficher l'image du joueur et de l'ordinateur dans les cercles blanc
+// C'est pour afficher l'image du joueur et de l'ordinateur dans les cercles blancs
 function afficherChoix(choixElem, choixType) {
     if (choixType === 'feu') {
         choixElem.style.backgroundImage = "url('https://www.giantbomb.com/a/uploads/scale_medium/16/164924/3083931-8746743194-flat%2C.jpg')";
-    } else if (choixType ==='eau') {
+    } else if (choixType === 'eau') {
         choixElem.style.backgroundImage = "url('https://ih0.redbubble.net/image.402379411.5464/flat,550x550,075,f.jpg')";
     } else if (choixType === 'plante') {
         choixElem.style.backgroundImage = "url('https://cdn.chaoscards.co.uk/uploads/prod_img/2_176677_s.png?v=-62169983925')";
@@ -32,23 +31,23 @@ function jouerJeu(choixJoueurType) {
     const choixOrdi = choixOrdinateurAleatoire();
 
     // choix de l'ordinateur
-    afficherChoix(choixordinateur, choixOrdi);
+    afficherChoix(choixjoueur, choixOrdi);
 
     // choix du joueur
-    afficherChoix(choixJoueur, choixJoueurType);
+    afficherChoix(choixordinateur, choixJoueurType);
 
     // pour déterminer le gagnant
     let resultat = "";
     if (choixJoueurType === choixOrdi) {
-        resultat = "Egalite !";
+        resultat = "Égalité !";
     } else if (
-        (choixJoueurType ==='feu' && choixOrdi ==='plante') ||
-        (choixJoueurType ==='eau' && choixOrdi ==='feu') ||
-        (choixJoueurType ==='plante' && choixOrdi ==='eau')
+        (choixJoueurType === 'feu' && choixOrdi === 'plante') ||
+        (choixJoueurType === 'eau' && choixOrdi === 'feu') ||
+        (choixJoueurType === 'plante' && choixOrdi === 'eau')
     ) {
         resultat = "Vous avez gagné !";
     } else {
-        resultat = "L'oridnateur a gagné !";
+        resultat = "L'ordinateur a gagné !";
     }
 
     alert(resultat); // afficher le résultat
@@ -67,6 +66,6 @@ plante.addEventListener('click', () => {
 
 // pour rejouer
 rejouer.addEventListener('click', () => {
-    choixJoueur.style.backgroundImage = "";
+    choixjoueur.style.backgroundImage = "";
     choixordinateur.style.backgroundImage = "";
 });
